ecabe80108a8d794921b6d71beed4f8ca61c6e4e
Niclas Jansen <niclas.jansen@gmail.com>
2023-10-02 18:06:05 +0200

    add trouble config

---
+++ after/plugin/trouble.lua
@@ -0,0 +1,4 @@
+local trouble = require("trouble")
+vim.keymap.set("n", "<leader>fq", function() trouble.open("quickfix") end, {desc="Trouble Quickfix", silent=true, noremap=true})
+vim.keymap.set("n", "<leader>fd", function() trouble.open("document_diagnostics") end, {desc="Trouble Document", silent=true, noremap=true})
+vim.keymap.set("n", "<leader>fw", function() trouble.open("workspace_diagnostics") end, {desc="Trouble Workspace", silent=true, noremap=true})
