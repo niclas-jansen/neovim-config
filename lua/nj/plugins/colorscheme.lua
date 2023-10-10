b68cd06d08de0cf4b851e268622eb8951f0b304c
Niclas Jansen <niclas.jansen@gmail.com>
2023-10-10 19:40:31 +0200

    added optional colorscheme config

--- lua/nj/plugins/colorscheme.lua
+++ lua/nj/plugins/colorscheme.lua
@@ -3,10 +3,22 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     config = function()
-      -- "rose-pine/neovim"
       vim.cmd("colorscheme catppuccin")
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   }
 }
+
+-- return {
+--   {
+--     "bluz71/vim-nightfly-guicolors",
+--     name = "nightfly",
+--     config = function()
+--       -- "rose-pine/neovim"
+--       vim.cmd("colorscheme nightfly")
+--       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--     end,
+--   }
+-- }
