9f71a1a7776b1f19ff92cb00ed5140da18524f46
Niclas Jansen <niclas.jansen@gmail.com>
2023-10-10 18:33:33 +0200

    formatting

--- lua/nj/plugins/oil.lua
+++ lua/nj/plugins/oil.lua
@@ -1,8 +1,13 @@
 return {
-	"stevearc/oil.nvim",
-	config = function()
-		require("oil").setup()
-		vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
-	dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+  "stevearc/oil.nvim",
+  enabled = true,
+  config = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      }
+    })
+    vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+  dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
 }
